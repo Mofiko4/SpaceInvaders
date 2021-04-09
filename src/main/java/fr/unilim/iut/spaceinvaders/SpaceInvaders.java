d8405af89afff48d7ab9349cff2ea31816dc0144
Léo Landelle <leo.landelle@etu.unilim.fr>
2021-04-09 08:51:25 +0200

    update

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -4,6 +4,7 @@ public class SpaceInvaders {
 
 	int longueur;
 	int hauteur;
+	Vaisseau vaisseau;
 
 	public SpaceInvaders(int longueur, int hauteur) {
 	   this.longueur = longueur;
@@ -15,10 +16,17 @@ public class SpaceInvaders {
 		StringBuilder espaceDeJeu = new StringBuilder();
 		for (int i = 0; i < hauteur; i++) {
 			for (int j = 0; j < longueur; j++) {
-				espaceDeJeu.append('.');
+				 if (vaisseau.occupeLaPosition(x, y))
+				     espaceDeJeu.append('V');
+				else
+					espaceDeJeu.append('.');
 			}
 			espaceDeJeu.append('\n');
 		}
 		return espaceDeJeu.toString();
+	}
+
+	public void positionnerUnNouveauVaisseau(int x, int y) {
+	        Vaisseau vaisseau = new Vaisseau (x,y);
 	}
 }
